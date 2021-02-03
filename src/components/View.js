aee1f5497b6e64ad322a86aab8e1b338d53f6101
jems-kick <71431280+jems-kick@users.noreply.github.com>
2021-02-03 15:48:04 +0530

    remove sr no.1

--- src/components/View.js
+++ src/components/View.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 const Exercise = (props) => (
     <tr>
         <td>{props.view.name}</td>
-        <td>{props.view.name}</td>
         <td>{props.view.title}</td>
         <td>{props.view.summary}</td>
         <td>
